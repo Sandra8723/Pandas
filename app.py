71ba4cebf01e941b229dc8e6a2132dcb6434e28e
sandra <sandrahiguitatoro@gmail.com>
2023-10-13 08:30:41 -0500

    c2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 
 data= pd.read_csv('ventas_vehiculos .csv')
 st.header('TABLA PRINCIPAL')
